cec1173015c6b08166fd1020aa29ed3c5345ec80
zakaria ben salek <zakariaazaraf@gmail.com>
2021-02-16 23:27:02 +0100

    render the header and start styling it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Header></Header>
       </header>
     </div>
   );
