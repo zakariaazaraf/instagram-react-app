30cacd9a9e6579ebfccf825f5f2d316b8dc0b152
zakaria ben salek <zakariaazaraf@gmail.com>
2021-02-16 23:18:16 +0100

    build the header compenent

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+
+function Header(){
+    return(
+        <header>
+            <a href="">instagram</a>
+            <form><input type='search' placeholder='search'></input></form>
+            <nav>
+                <ul>
+                    <li><a href=''></a>home</li>
+                    <li><a href=''></a>share</li>
+                    <li><a href=''></a>pass</li>
+                    <li><a href=''></a>notification</li>
+                    <li><a href=''></a>profile</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
